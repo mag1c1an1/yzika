04df8e4345f2b8dd7f7f62fc62aa28dd88071b00
mag1c1an1 <mag1cian@icloud.com>
2026-07-01 18:27:00 +0800

    feat: basic

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "MaG1ciaN";
+export const SITE_TITLE = "白日梦想家";
+export const SITE_TITLE_EN = "UndefinedBehavior Lab";
+export const SITE_AUTHOR = "MaG1ciaN";
+export const SITE_GITHUB = "mag1c1an1";
+export const SITE_SINCE = 2026;
 export const SITE_DESCRIPTION = "Welcome to my website!";
